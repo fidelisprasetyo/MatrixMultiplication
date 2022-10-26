c20bfa8f4330a9a6a405e3d94409d4871f9ddc6d
fidelisprasetyo <fidelisprasetyo@gmail.com>
2022-10-26 02:21:55 -0700

    brute force multiplication function added

--- SquareMatrix.h
+++ SquareMatrix.h
@@ -1,34 +1,65 @@
 #pragma once
 
+#include <iomanip>
+#include <stdlib.h>
+
+#define RAND_RANGE 10
+
+int getRandInt(int range) {
+    return rand() % range;
+}
+
 class SquareMatrix {
 public:
-    SquareMatrix(int _size) : size(_size) {
-        m.resize(size);
-        for(int i = 0; i < size; i++) {
-            m[i].resize(size);
-        }
-    }
-    void insertValues() {
-        std::cout << "Enter " << size << " values per row (separated by spaces)\n";
-        for(int row = 0; row < size; row++) {
-                std::cout << "Row " << row << " values: ";
-            for(int col = 0; col < size; col++) {
-                std::cin >> m[row][col];
-            }
-        }
-    }
-    void printMatrix() const {
-        for(int row = 0; row < size; row++) {
-            for(int col = 0; col < size; col++) {
-                std::cout << m[row][col] << ' ';
-            }
-            std::cout << '\n';
-        }
-    }
-    int getSize() const {
-        return size;
-    }
+    SquareMatrix(int _size);
+    void insertValues();
+    void randomValues();
+    void print() const;
+    int getSize() const;
+    std::vector<int>& operator [](int i);
 private:
     int size;
     std::vector<std::vector<int>> m;
 };
+
+SquareMatrix::SquareMatrix(int _size) : size(_size) {
+    m.resize(size);
+    for(int i = 0; i < size; i++) {
+        m[i].resize(size);
+    }
+}
+
+void SquareMatrix::insertValues() {
+    std::cout << "Enter " << size << " values per row (separated by spaces)\n";
+    for(int row = 0; row < size; row++) {
+            std::cout << "Row " << row << " values: ";
+        for(int col = 0; col < size; col++) {
+            std::cin >> m[row][col];
+        }
+    }
+}
+
+void SquareMatrix::randomValues() {
+    for(int row = 0; row < size; row++) {
+        for(int col = 0; col < size; col++) {
+            m[row][col] = getRandInt(RAND_RANGE);
+        }
+    }
+}
+
+void SquareMatrix::print() const {
+    for(int row = 0; row < size; row++) {
+        for(int col = 0; col < size; col++) {
+            std::cout << std::setw(3) << m[row][col] << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+int SquareMatrix::getSize() const {
+    return size;
+}
+
+std::vector<int>& SquareMatrix::operator [](int i) {
+    return m[i];
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+// Author: Fidelis Prasetyo (fprasetyo@cpp.edu)
+// Desc: for Project 1, CS3310 Fall, Cal Poly Pomona 
+// github: https://github.com/fidelisprasetyo
+// github repo: https://github.com/fidelisprasetyo/MatrixMultiplication
+
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -10,18 +15,35 @@ bool isPowerOfTwo(int n) {
     return (ceil(log2(n)) == floor(log2(n)));
 }
 
-void bruteForceMul(const SquareMatrix& M1, const SquareMatrix& M2) {
+bool isMatrixValid(const SquareMatrix& M1, const SquareMatrix& M2) {
     if(M1.getSize() != M2.getSize() || !isPowerOfTwo(M1.getSize()) || !isPowerOfTwo(M2.getSize())) {
-        std::cout << "Invalid matrix sizes!\n";
-        return;
+        return false;
     }
-    std::cout << "Multiplication!\n";
+    return true;
+}
+
+void bruteForceMul(SquareMatrix& M1, SquareMatrix& M2) {
+    int n = M1.getSize();
+    SquareMatrix P(n);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            P[i][j] = 0;
+            for(int k = 0; k < n; k++) {
+                P[i][j] += M1[i][k] * M2[k][j];
+            }
+        }
+    }
+    P.print();
     return;
 }    
 
 int main() {
-    SquareMatrix M1(5);
-    SquareMatrix M2(5);
-    bruteForceMul(M1, M2);
+    srand(time(NULL));
+    SquareMatrix M1(4);
+    SquareMatrix M2(4);
+    M1.randomValues();
+    M2.randomValues();
+    M1.print();
+    M2.print();
     return 0;
 }
